cdf9f27c3185d6407ebb6539dd89fb0087bd501c
Larry Gordon <lgordon@psyrendust.com>
2013-07-02 15:51:58 -0700

    Bug fix

--- plugins/npp/npp.plugin.zsh
+++ plugins/npp/npp.plugin.zsh
@@ -2,9 +2,9 @@ _npp_win_paths=(
   "/cygdrive/c/Program Files/Notepad++/notepad++.exe"
   "/cygdrive/c/Program Files (x86)/Notepad++/notepad++.exe"
 )
-for _sublime_path in $_sublime_win_paths; do
-  if [[ -a $_sublime_path ]]; then
-    NPP_APP="$_sublime_path"
+for _npp_path in $_npp_win_paths; do
+  if [[ -a $_npp_path ]]; then
+    NPP_APP="$_npp_path"
     break
   fi
 done
