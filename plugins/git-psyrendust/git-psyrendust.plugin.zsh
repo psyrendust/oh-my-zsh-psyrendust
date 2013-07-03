b0987e173f1e9820b6bfd05d0d6c6001f7680ebd
Larry Gordon <lgordon@psyrendust.com>
2013-07-03 03:04:45 -0700

    Updated git aliases.

--- plugins/git-psyrendust/git-psyrendust.plugin.zsh
+++ plugins/git-psyrendust/git-psyrendust.plugin.zsh
@@ -8,3 +8,9 @@ compdef _git gaa=git-add
 
 alias gun='git reset && git checkout . && git clean -fdx'
 compdef _git gun=git-reset
+
+alias gt='git tag'
+compdef _git gt=git-tag
+
+alias gta='git tag -a'
+compdef _git gt=git-tag
