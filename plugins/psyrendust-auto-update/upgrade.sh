457ce24e2933e6200c68176a9528738768b650f4
Larry Gordon <larry.gordon@xero.com>
2014-01-15 15:46:05 -0800

    Fixing logic for upgrade.sh

--- plugins/psyrendust-auto-update/upgrade.sh
+++ plugins/psyrendust-auto-update/upgrade.sh
@@ -1,98 +1,110 @@
-function isgitrepoandcanupdate() {
+function isgitrepo() {
   # check if we're in a git repo
-  command git rev-parse --is-inside-work-tree &>/dev/null || return 0
-  # check if it's dirty
-  command git diff --quiet --ignore-submodules HEAD &>/dev/null
-  if [[ $? == 1 ]]; then
-    git reset HEAD --hard;
-    return 1
+  if [[ $(git rev-parse --is-inside-work-tree) == "true" ]]; then
+    echo 1;
   else
-    return 0
+    echo 0;
+  fi
+}
+
+function cleanup() {
+  # check if it's dirty and reset it back to HEAD
+  if [[ -n $(git diff --ignore-submodules HEAD) ]]; then
+    git reset HEAD --hard
   fi
 }
 
 function updatepsyrendust() {
-  printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh Psyrendust"
   cd "$ZSH_CUSTOM"
   # Cleanup our repo with a stash save in case someone was mucking around
-  if [[ isgitrepoandcanupdate == 1 ]]; then
+  if [[ $(isgitrepo) == 1 ]]; then
+    cleanup
     if git pull --rebase origin master; then
       # Copy over any template updates
       cp $ZSH_CUSTOM/templates/zshrc.zsh-template ~/.zshrc
-      printf '\033[0;34m%s\033[0m\n' 'Oh My Zsh Psyrendust update successful!'
-      return 1
     else
-      return 0
+      echo 0
     fi
-  else
-    return 0
   fi
+  echo 1
 }
 
 function updatepuretheme() {
   # Update the pure theme from source
   if [[ $ZSH_THEME == "pure" && $PURE_THEME == "$HOME/.pure-theme" ]]; then
-    printf '\033[0;34m%s\033[0m\n' "Upgrading Pure theme"
     cd "$PURE_THEME"
-    isgitrepoandcanupdate
-    if git pull --rebase origin master; then
-      ln -sf $HOME/.pure-theme/pure.zsh $ZSH_CUSTOM/themes/pure.zsh-theme
-      printf '\033[0;34m%s\033[0m\n' 'Pure theme update successful!'
-      return 1
-    else
-      return 0
+    if [[ $(isgitrepo) == 1 ]]; then
+      cleanup
+      if git pull --rebase origin master; then
+        ln -sf $HOME/.pure-theme/pure.zsh $ZSH_CUSTOM/themes/pure.zsh-theme
+      else
+        echo 0
+      fi
     fi
-  else
-    return 1
   fi
+  echo 1
 }
 
 function updatezshrcpersonal() {
   # Update custom personal zshrc
   if [[ -s "${ZSHRC_PERSONAL}/.zshrc" ]]; then
-    printf '\033[0;34m%s\033[0m\n' "Upgrading personal zshrc"
     cd "${ZSHRC_PERSONAL}"
-    if [[ isgitrepoandcanupdate == 1 ]]; then
+    if [[ $(isgitrepo) == 1 ]]; then
+      cleanup
       if git pull --rebase origin master; then
-        printf '\033[0;34m%s\033[0m\n' 'Personal zshrc update successful!'
-        return 1
+        echo 1
       else
-        return 0
+        echo 0
       fi
-    else
-      return 0
     fi
-  else
-    return 1
   fi
+  echo 1
 }
 
 function updatezshrcwork() {
   # Update custom personal zshrc
   if [[ -s "${ZSHRC_WORK}/.zshrc" ]]; then
-    printf '\033[0;34m%s\033[0m\n' "Upgrading work zshrc"
     cd "${ZSHRC_WORK}"
-    if [[ isgitrepoandcanupdate == 1 ]]; then
+    if [[ $(isgitrepo) == 1 ]]; then
+      cleanup
       if git pull --rebase origin master; then
-        printf '\033[0;34m%s\033[0m\n' 'Work zshrc update successful!'
-        return 1
+        echo 1
       else
-        return 0
+        echo 0
       fi
-    else
-      return 0
     fi
-  else
-    return 1
   fi
+  echo 1
 }
 
-function processupgrade() {
-  local successupdatepsyrendust=updatepsyrendust;
-  local successupdatepuretheme=updatepuretheme;
-  local successupdatezshrcpersonal=updatezshrcpersonal;
-  local successupdatezshrcpersonal=updatezshrcpersonal;
-  if [[ successupdatepsyrendust == 1 && successupdatepuretheme == 1 && successupdatezshrcpersonal == 1 && successupdatezshrcpersonal == 1 ]]; then
+function processupdate() {
+  printf '\033[0;34m%s\033[0m\n' "process update..."
+
+  printf '\033[0;34m%s\033[0m\n' "Updating Oh My Zsh Psyrendust"
+  successupdatepsyrendust=$(updatepsyrendust)
+  if [[ $successupdatepsyrendust == *1* ]]; then
+    printf '\033[0;34m%s\033[0m\n' 'Oh My Zsh Psyrendust update successful!';
+  fi
+
+  printf '\033[0;34m%s\033[0m\n' "Updating Pure Theme"
+  successupdatepuretheme=$(updatepuretheme)
+  if [[ $successupdatepuretheme == *1* ]]; then
+    printf '\033[0;34m%s\033[0m\n' 'Pure theme update successful!'
+  fi
+
+  printf '\033[0;34m%s\033[0m\n' "Updating Personal zshrc"
+  successupdatezshrcpersonal=$(updatezshrcpersonal)
+  if [[ $successupdatezshrcpersonal == *1* ]]; then
+    printf '\033[0;34m%s\033[0m\n' 'Personal zshrc update successful!'
+  fi
+
+  printf '\033[0;34m%s\033[0m\n' "Updating Work zshrc"
+  successupdatezshrcwork=$(updatezshrcwork)
+  if [[ $successupdatezshrcwork == *1* ]]; then
+    printf '\033[0;34m%s\033[0m\n' 'Work zshrc update successful!'
+  fi
+
+  if [[ $successupdatepsyrendust == *1* && $successupdatepuretheme == *1* && $successupdatezshrcpersonal == *1* && $successupdatezshrcwork == *1* ]]; then
     printf '\033[0;32m%s\033[0m\n' '       __                            __                                    __         __  '
     printf '\033[0;32m%s\033[0m\n' ' ___  / /     __ _  __ __   ___ ___ / /     ___  ___ __ _________ ___  ___/ /_ _____ / /_ '
     printf '\033[0;32m%s\033[0m\n' '/ _ \/ _ \   /  ` \/ // /  /_ /(_-</ _ \   / _ \(_-</ // / __/ -_) _ \/ _  / // (_-</ __/ '
@@ -100,21 +112,24 @@ function processupgrade() {
     printf '\033[0;32m%s\033[0m\n' '                  /___/                  /_/       /___/                                  '
     printf '\033[0;34m%s\033[0m\n' 'Hooray! Oh My Zsh Psyrendust has been updated!.'
   else
-    if [[ successupdatepsyrendust == 0 ]]; then
+    if [[ $successupdatepsyrendust == *0* ]]; then
       printf '\033[0;31m%s\033[0m\n' 'There was an error updating Oh My Zsh Psyrendust.'
     fi
-    if [[ successupdatepuretheme == 0 ]]; then
+    if [[ $successupdatepuretheme == *0* ]]; then
       printf '\033[0;31m%s\033[0m\n' 'There was an error updating Pure Theme.'
     fi
-    if [[ successupdatezshrcpersonal == 0 ]]; then
+    if [[ $successupdatezshrcpersonal == *0* ]]; then
       printf '\033[0;31m%s\033[0m\n' 'There was an error updating personal zshrc.'
     fi
-    if [[ successupdatezshrcpersonal == 0 ]]; then
+    if [[ $successupdatezshrcwork == *0* ]]; then
       printf '\033[0;31m%s\033[0m\n' 'There was an error updating work zshrc.'
     fi
     printf '\033[0;31m%s\033[0m\n' 'Please try again later.'
-
   fi
+  unset successupdatepsyrendust;
+  unset successupdatepuretheme;
+  unset successupdatezshrcpersonal;
+  unset successupdatezshrcwork;
 }
 
-processupgrade
+processupdate
