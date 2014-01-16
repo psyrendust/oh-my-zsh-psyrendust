6fe0b9eca3d98eb9325e4571c5f2d18c827608ec
Larry Gordon <larry.gordon@xero.com>
2014-01-16 10:37:31 -0800

    Updates to paths and coding conventions

--- setup/bootstrap-mac-shell.sh
+++ setup/bootstrap-mac-shell.sh
@@ -8,48 +8,59 @@
 # paste each command at your leisure
 
 if [[ "$(echo $PATH)" != */usr/local/bin* ]]; then
-  export PATH="/usr/local/bin:${PATH}"
+  export PATH="/usr/local/bin:${PATH}";
 fi
 
 # Check and see if node is installed.
 if [[ -s /usr/local/bin/node ]]; then
-  echo "Great, you have node installed!"
+  echo "Great, you have node installed!";
 else
   # Node is not installed send the user to go install it
-  echo "You don't have node installed!"
-  echo "Press [ENTER] to open your browser to get the installer. Then return here and run this script again."
-  open "http://nodejs.org/download/"
-  exit
+  echo "You don't have node installed!";
+  echo "Press [ENTER] to open your browser to get the installer. Then return here and run this script again.";
+  read youneednode;
+  open "http://nodejs.org/download/";
+  exit;
 fi
 
-echo "Checking for homebrew"
+# Grab some user info
+echo "Please enter your first and last name [First Last]: ";
+read gitusername;
+echo "Please enter your work email address [first.last@xero.com]: ";
+read gituseremail;
+
+echo "Checking for homebrew..."
 # Install Homebrew
 if [[ $(which -s brew) != 0 ]]; then
-    echo "Homebrew missing"
+    echo "Homebrew missing";
     # Install Homebrew
     # https://github.com/mxcl/homebrew/wiki/installation
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)";
 else
-    echo "Homebrew installed"
+    echo "Homebrew installed";
     # Make sure we’re using the latest Homebrew
-    brew update
+    brew update;
     # Upgrade any already-installed formulae
-    brew upgrade
+    brew upgrade;
 fi
 
 # Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
+brew install coreutils;
 # Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to $PATH.
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
+brew install findutils;
 # Install the latest Bash and Zsh
-brew install bash zsh
-# Add it to the allowed shells list
-sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
-sudo bash -c "echo /usr/local/bin/zsh >> /private/etc/shells"
+brew install bash zsh;
+# Add it to the allowed shells list if it's not already there
+if [[ -z $(cat /private/etc/shells | grep "/usr/local/bin/bash") ]]; then
+  sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells";
+fi
+if [[ -z $(cat /private/etc/shells | grep "/usr/local/bin/zsh") ]]; then
+  sudo bash -c "echo /usr/local/bin/zsh >> /private/etc/shells";
+fi
 # Change root shell to the new zsh
-sudo chsh -s /usr/local/bin/zsh
+sudo chsh -s /usr/local/bin/zsh;
 
 # Make sure that everything went well
 echo "SHELL: $SHELL";
@@ -57,69 +68,79 @@ echo "$BASH_VERSION";
 echo $(zsh --version);
 
 # Install wget with IRI support
-brew install wget --enable-iri
+brew install wget --enable-iri;
 
 # Install more recent versions of some OS X tools
-brew tap homebrew/dupes && brew install homebrew/dupes/grep --default-names
+brew tap homebrew/dupes && brew install homebrew/dupes/grep --default-names;
 
 # Install everything else
-brew install ack automake curl-ca-bundle fasd git optipng phantomjs rename tree
+brew install ack automake curl-ca-bundle fasd git optipng phantomjs rename tree;
 
 # Remove outdated versions from the cellar
-brew cleanup
+brew cleanup;
 
 # Setup your zsh prompt and plugins
 # a little cleanup
-rm ~/.zcompdump*;
-rm ~/.zlogin;
-rm ~/.zsh-update;
-rm ~/.zsh_history;
+rm "${HOME}/.zcompdump*";
+rm "${HOME}/.zlogin";
+rm "${HOME}/.zsh-update";
+rm "${HOME}/.zsh_history";
 
 # install oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh;
+git clone https://github.com/robbyrussell/oh-my-zsh.git "${HOME}/.oh-my-zsh";
 chsh -s /usr/local/bin/zsh;
 
 # install some oh-my-zsh extras
-git clone https://github.com/psyrendust/oh-my-zsh-psyrendust.git ~/.oh-my-zsh-psyrendust;
-cp ~/.oh-my-zsh-psyrendust/templates/zshrc.zsh-template ~/.zshrc;
-cp ~/.oh-my-zsh-psyrendust/templates/gemrc.zsh-template ~/.gemrc;
-cp ~/.oh-my-zsh-psyrendust/templates/gitconfig.template ~/.gitconfig;
-cp ~/.oh-my-zsh-psyrendust/templates/gitignore_global.template ~/.gitignore_global;
+git clone https://github.com/psyrendust/oh-my-zsh-psyrendust.git "${HOME}/.oh-my-zsh-psyrendust";
+cp "${HOME}/.oh-my-zsh-psyrendust/templates/zshrc.zsh-template" "${HOME}/.zshrc";
+cp "${HOME}/.oh-my-zsh-psyrendust/templates/gemrc.zsh-template" "${HOME}/.gemrc";
+cp "${HOME}/.oh-my-zsh-psyrendust/templates/gitconfig.template" "${HOME}/.gitconfig";
+cp "${HOME}/.oh-my-zsh-psyrendust/templates/gitignore_global.template" "${HOME}/.gitignore_global";
+
+# Set Git user info
+git config --global user.name $gitusername;
+git config --global user.email $gituseremail;
+
+# Copy over fonts
+[[ -d "${HOME}/Library/Fonts" ]] || mkdir -p "${HOME}/Library/Fonts";
+cp "${HOME}/.oh-my-zsh-psyrendust/fonts/." "${HOME}/Library/Fonts/";
 
 # install the pure theme
-git clone https://github.com/sindresorhus/pure.git ~/.pure-theme;
+git clone https://github.com/sindresorhus/pure.git "${HOME}/.pure-theme";
 
 # symlink the pure theme to our oh-my-zsh-psyrendust custom theme folder
-ln -sf ~/.pure-theme/pure.zsh ~/.oh-my-zsh-psyrendust/themes/pure.zsh-theme;
+ln -sf "${HOME}/.pure-theme/pure.zsh" "${HOME}/.oh-my-zsh-psyrendust/themes/pure.zsh-theme";
+
+source "${HOME}/.zshrc";
 
 # install zshrc-work
-git clone https://github.dev.xero.com/dev-larryg/zshrc-xero.git ~/.zshrc-work;
+git clone https://github.dev.xero.com/dev-larryg/zshrc-xero.git "${HOME}/.zshrc-work";
 
 # install zshrc-personal starter template
-mkdir ~/.zshrc-personal
-cp ~/.oh-my-zsh-psyrendust/templates/zshrc-personal.zsh-template ~/.zshrc-personal/.zshrc
+mkdir "${HOME}/.zshrc-personal";
+cp "${HOME}/.oh-my-zsh-psyrendust/templates/zshrc-personal.zsh-template" "${HOME}/.zshrc-personal/.zshrc";
 
 # install iTerm2
 #
 # install the tomorrow-theme for iTerm2
-# git clone https://github.com/chriskempson/tomorrow-theme.git ~/.tomorrow-theme;
+# git clone https://github.com/chriskempson/tomorrow-theme.git "${HOME}/.tomorrow-theme";
 # try to do some applescript kung-fu to auto install the theme into iTerm2
 
 # install a default hosts file
-sudo cp ~/.zshrc-work/templates/hosts /etc/hosts;
+sudo cp "${HOME}/.zshrc-work/templates/hosts" "/etc/hosts";
 
 # add some automount sugar for Parallels
-sudo cp ~/.zshrc-work/templates/auto_master /private/etc/auto_master;
-sudo cp ~/.zshrc-work/templates/auto_smb /private/etc/auto_smb;
+sudo cp "${HOME}/.zshrc-work/templates/auto_master" "/private/etc/auto_master";
+sudo cp "${HOME}/.zshrc-work/templates/auto_smb" "/private/etc/auto_smb";
 
-source ~/.zshrc
+source "${HOME}/.zshrc";
 
 # let's do some admin type stuff
 # add myself to wheel group
-sudo dseditgroup -o edit -a $(echo $USER) -t user wheel
+sudo dseditgroup -o edit -a $(echo $USER) -t user wheel;
 
 # Change ownership of /usr/local
-sudo chown -R $(echo $USER):admin /usr/local
+sudo chown -R $(echo $USER):admin /usr/local;
 
 # https://rvm.io
 # rvm for the rubiess
@@ -127,13 +148,21 @@ curl -L https://get.rvm.io | bash -s stable --ruby;
 
 # To start using RVM you need to run `source "/Users/$USER/.rvm/scripts/rvm"`
 # in all your open shell windows, in rare cases you need to reopen all shell windows.
-source ~/.zshrc
+source "${HOME}/.zshrc";
 
+rvm get stable;               # Update rvm
+rvm reload;                   # Reload the updated version of rvm
 rvm install 2.0.0;
 rvm --default 2.0.0
 ruby -v;
 which ruby;
+gem update --system;
 gem install rails;
+gem install bundler;
+gem install compass -v 1.0.0.alpha.13;
+gem install sass -v 3.3.0.rc2;
+
+rvm cleanup all;
 
 # Install Compass
 # http://compass-style.org/install/
@@ -160,22 +189,22 @@ gem install sass -v 3.3.0.rc2;
 npm install -g bower grunt-init grunt-cli jshint;
 
 # install some templates
-mkdir ~/.grunt-init;
-git clone https://github.com/gruntjs/grunt-init-gruntfile.git ~/.grunt-init/gruntfile;
-git clone https://github.com/gruntjs/grunt-init-commonjs.git ~/.grunt-init/commonjs;
-git clone https://github.com/gruntjs/grunt-init-gruntplugin.git ~/.grunt-init/gruntplugin;
-git clone https://github.com/gruntjs/grunt-init-jquery.git ~/.grunt-init/jquery;
-git clone https://github.com/gruntjs/grunt-init-node.git ~/.grunt-init/node;
+mkdir "${HOME}/.grunt-init";
+git clone https://github.com/gruntjs/grunt-init-gruntfile.git "${HOME}/.grunt-init/gruntfile";
+git clone https://github.com/gruntjs/grunt-init-commonjs.git "${HOME}/.grunt-init/commonjs";
+git clone https://github.com/gruntjs/grunt-init-gruntplugin.git "${HOME}/.grunt-init/gruntplugin";
+git clone https://github.com/gruntjs/grunt-init-jquery.git "${HOME}/.grunt-init/jquery";
+git clone https://github.com/gruntjs/grunt-init-node.git "${HOME}/.grunt-init/node";
 
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments;
 
-source ~/.zshrc
+source "${HOME}/.zshrc"
 
-echo "Don't forget to:"
-echo "1. Setup Parallels to autostart on login."
-echo "2. Set Parallels Shared Network DHCP Settings."
-echo "   Start Address: 1.2.3.1"
-echo "   End Address  : 1.2.3.254"
-echo "   Subnet Mask  : 255.255.255.0"
-echo "Restart your computer now."
+echo "Don't forget to:";
+echo "1. Setup Parallels to autostart on login.";
+echo "2. Set Parallels Shared Network DHCP Settings.";
+echo "   Start Address: 1.2.3.1";
+echo "   End Address  : 1.2.3.254";
+echo "   Subnet Mask  : 255.255.255.0";
+echo "Restart your computer now.";
