32658c42c7fabcb206480050f9ba9feabeafa3fc
Larry Gordon <larry.gordon@xero.com>
2014-01-17 17:38:26 -0800

    Last of the tweaks to patch-mac-mavericks-samba.sh

--- setup/patch-mac-mavericks-samba.sh
+++ setup/patch-mac-mavericks-samba.sh
@@ -40,7 +40,11 @@
 #
 # Untick the checkbox for File Sharing
 # ----------------------------------------------------------
-
+printf '\033[0;32m%s\033[0m\n' '1. Start off by turning off File Sharing. Press enter to open System Preferences [Enter]: ';
+read foo;
+open /System/Library/PreferencePanes/SharingPref.prefPane;
+printf '\033[0;32m%s\033[0m\n' 'Press enter to continue [Enter]: ';
+read foo;
 
 
 # ----------------------------------------------------------
@@ -51,6 +55,28 @@
 #
 # Edit /etc/services to do this:
 # ----------------------------------------------------------
+printf '\033[0;32m%s\033[0m\n' '2. Move the current service ports that OS X Samba uses to a different port';
+printf '\033[0;32m%s\033[0m\n' 'Modify the entries with the following port numbers 137, 138, 139 and 445 and prepend 19 to them';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ns      137/udp     # NETBIOS Name Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ns      137/tcp     # NETBIOS Name Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-dgm     138/udp     # NETBIOS Datagram Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-dgm     138/tcp     # NETBIOS Datagram Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ssn     139/udp     # NETBIOS Session Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ssn     139/tcp     # NETBIOS Session Service';
+printf '\033[0;32m%s\033[0m\n' '    ...';
+printf '\033[0;32m%s\033[0m\n' '    microsoft-ds    445/udp     # Microsoft-DS';
+printf '\033[0;32m%s\033[0m\n' '    microsoft-ds    445/tcp     # Microsoft-DS';
+printf '\033[0;32m%s\033[0m\n' '\nResults should look like the following:';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ns      137/udp     # NETBIOS Name Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ns      137/tcp     # NETBIOS Name Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-dgm     138/udp     # NETBIOS Datagram Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-dgm     138/tcp     # NETBIOS Datagram Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ssn     139/udp     # NETBIOS Session Service';
+printf '\033[0;32m%s\033[0m\n' '    netbios-ssn     139/tcp     # NETBIOS Session Service';
+printf '\033[0;32m%s\033[0m\n' '    ...';
+printf '\033[0;32m%s\033[0m\n' '    microsoft-ds    445/udp     # Microsoft-DS';
+printf '\033[0;32m%s\033[0m\n' '    microsoft-ds    445/tcp     # Microsoft-DS';
+printf '\033[0;32m%s\033[0m\n' 'Press enter to edit /etc/services [Enter]: ';
 sudo nano /etc/services;
 
 # ----------------------------------------------------------
@@ -86,9 +112,13 @@ sudo nano /etc/services;
 # 3. Uninstall Samba if it's already there and then install
 # our version using Homebrew.
 # ----------------------------------------------------------
-brew list | grep samba;
-if [[ $? != 0 ]]; then
+printf '\033[0;32m%s\033[0m\n' '3. Uninstall Samba if its already there and then install our version using Homebrew';
+if [[ -n $(brew list | grep autogen) ]]; then
+  brew uninstall autogen;
+fi
+if [[ -n $(brew list | grep samba) ]]; then
   brew uninstall samba;
+  rm "$(brew --cache)/"samba*;
 fi
 brew doctor;
 brew update;
@@ -102,8 +132,12 @@ brew cleanup;
 # 4. Install system LaunchDaemons to launch the new version
 # of Samba.
 # ----------------------------------------------------------
-curl -s https://gist.github.com/psyrendust/8466620/raw/7f814423920ddc0f53da895d408c6e1ed922b023/org.samba.nmbd.plist | xargs -0 echo > "/Library/LaunchDaemons/org.samba.nmbd.plist";
-curl -s https://gist.github.com/psyrendust/8466620/raw/a5d4aedf029c7a90181d67d431cf3bcea1c8bf2a/org.samba.smbd.plist | xargs -0 echo > "/Library/LaunchDaemons/org.samba.smbd.plist";
+printf '\033[0;32m%s\033[0m\n' '4. Install system LaunchDaemons to launch the new version of Samba';
+curl -s https://gist.github.com/psyrendust/8466620/raw/7f814423920ddc0f53da895d408c6e1ed922b023/org.samba.nmbd.plist | xargs -0 echo > "${HOME}/Downloads/org.samba.nmbd.plist";
+curl -s https://gist.github.com/psyrendust/8466620/raw/a5d4aedf029c7a90181d67d431cf3bcea1c8bf2a/org.samba.smbd.plist | xargs -0 echo > "${HOME}/Downloads/org.samba.smbd.plist";
+
+sudo mv "${HOME}/Downloads/org.samba.nmbd.plist" "/Library/LaunchDaemons/org.samba.nmbd.plist";
+sudo mv "${HOME}/Downloads/org.samba.smbd.plist" "/Library/LaunchDaemons/org.samba.smbd.plist";
 
 
 
@@ -111,8 +145,9 @@ curl -s https://gist.github.com/psyrendust/8466620/raw/a5d4aedf029c7a90181d67d43
 # 5. Fix the owner and permissions so that OS X doesn't have
 # any issues trying to launch these plist files.
 # ----------------------------------------------------------
-sudo chown root:wheel "/Library/LaunchDaemons/org.samba.*";
-sudo chmod 744 "/Library/LaunchDaemons/org.samba.*";
+printf '\033[0;32m%s\033[0m\n' '5. Fix the owner and permissions so that OS X doesnt have any issues trying to launch these plist files';
+sudo chown root:wheel "/Library/LaunchDaemons/org.samba."*;
+sudo chmod 744 "/Library/LaunchDaemons/org.samba."*;
 
 
 
@@ -121,6 +156,7 @@ sudo chmod 744 "/Library/LaunchDaemons/org.samba.*";
 # version number of Samba we are installing.
 # ----------------------------------------------------------
 smbversion="3.6.20";
+printf '\033[0;32m%s\033[0m\n' '6. Installing SMB Version ${smbversion}';
 
 
 
@@ -128,6 +164,7 @@ smbversion="3.6.20";
 # 7. Create a private folder to store Samba's private
 # passwords if it doesn't exist.
 # ----------------------------------------------------------
+printf '\033[0;32m%s\033[0m\n' '7. Create a private folder to store Samba private passwords if it doesnt exist';
 [[ -d "/usr/local/Cellar/samba/${smbversion}/private" ]] || mkdir "/usr/local/Cellar/samba/${smbversion}/private";
 
 
@@ -150,13 +187,16 @@ smbversion="3.6.20";
 # - [c]         : name of the folder you are mounting
 # - path        : location of your local path you are mounting to
 # - valid users : username that has access to the share
+printf '\033[0;32m%s\033[0m\n' '8. Setting up a smb.conf starter template';
 curl -s https://gist.github.com/psyrendust/8466620/raw/7315c73c709cf48f8af88d88fc243b921847d515/smb.conf | xargs -0 echo > "/usr/local/etc/smb_custom.conf";
 
 # Change the owner, group and permissions
+printf '\033[0;32m%s\033[0m\n' '    - Change the owner, group and permissions';
 sudo chown $(echo $USER):admin "/usr/local/etc/smb_custom.conf";
 sudo chmod 755 "/usr/local/etc/smb_custom.conf";
 
 # Create the symlinks
+printf '\033[0;32m%s\033[0m\n' '    - Create the symlinks';
 ln -sf "/usr/local/etc/smb_custom.conf" "/usr/local/etc/smb.conf";
 ln -sf "/usr/local/etc/smb_custom.conf" "/usr/local/Cellar/samba/${smbversion}/etc/smb.conf";
 
@@ -167,14 +207,6 @@ ln -sf "/usr/local/etc/smb_custom.conf" "/usr/local/Cellar/samba/${smbversion}/e
 # 9. Set our new Samba service to launch when the computer
 # starts.
 # ----------------------------------------------------------
+printf '\033[0;32m%s\033[0m\n' '9. Set our new Samba service to launch when the computer starts';
 sudo launchctl load /Library/LaunchDaemons/org.samba.smbd.plist;
 sudo launchctl load /Library/LaunchDaemons/org.samba.nmbd.plist;
-
-
-
-
-
-
-
-
-
