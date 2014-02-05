d6b5c4d97192af5716b16ba2472a7d1a57d595b7
Larry Gordon <larry.gordon@xero.com>
2014-02-04 16:49:25 -0800

    Adds portable shebang and updates vars and references.

--- plugins/psyrendust-auto-update/psyrendust-auto-update.plugin.zsh
+++ plugins/psyrendust-auto-update/psyrendust-auto-update.plugin.zsh
@@ -1,5 +1,5 @@
+#!/usr/bin/env zsh
 # Check for updates on initial load...
-if [ "$DISABLE_AUTO_UPDATE" != "true" ]
-then
-  /usr/bin/env ZSH=$ZSH ZSH_CUSTOM=$ZSH_CUSTOM zsh $ZSH_CUSTOM/plugins/psyrendust-auto-update/check_for_upgrade.sh
+if [[ "$DISABLE_PSYRENDUST_AUTO_UPDATE" != "true" ]]; then
+  /usr/bin/env ZSH=$ZSH ZSH_CUSTOM=$ZSH_CUSTOM zsh $ZSH_CUSTOM/plugins/psyrendust-auto-update/check_for_upgrade.zsh
 fi
