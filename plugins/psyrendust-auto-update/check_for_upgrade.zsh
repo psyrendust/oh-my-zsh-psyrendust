71310b420d5441680377ae002be0c570094fc376
Larry Gordon <larry.gordon@xero.com>
2014-02-04 18:10:57 -0800

    Updates logic for checking git SHA's in check_for_upgrade.zsh

--- plugins/psyrendust-auto-update/check_for_upgrade.zsh
+++ plugins/psyrendust-auto-update/check_for_upgrade.zsh
@@ -3,23 +3,23 @@ function check_internet() {
   wget -q --tries=10 --timeout=5 http://www.google.com -O /tmp/index.google &> /dev/null && echo 1
 }
 
-function current_psyrendust_local_sha() {
+function get_current_psyrendust_local_sha() {
   cd "$ZSH_CUSTOM"
   echo $(git rev-parse HEAD)
 }
 
-function current_psyrendust_remote_sha() {
+function get_current_psyrendust_remote_sha() {
   echo $(git ls-remote $(git config remote.origin.url) HEAD | awk '{print $1}')
 }
 
-function update_psyrendust_remote_sha() {
-  echo "LAST_PSYRENDUST_REMOTE_SHA=$(current_psyrendust_remote_sha)" > "$HOME/.psyrendust-update"
+function update_psyrendust_local_sha() {
+  echo "CURRENT_PSYRENDUST_LOCAL_SHA=$(get_current_psyrendust_local_sha)" > "$HOME/.psyrendust-update"
 }
 
 function upgrade_psyrendust() {
   /usr/bin/env ZSH=$ZSH ZSH_CUSTOM=$ZSH_CUSTOM /bin/sh $ZSH_CUSTOM/plugins/psyrendust-auto-update/upgrade.zsh
   # update the psyrendust file
-  update_psyrendust_remote_sha
+  update_psyrendust_local_sha
 }
 
 function check_for_upgrade() {
@@ -29,17 +29,19 @@ function check_for_upgrade() {
     if [[ -f "$HOME/.psyrendust-update" ]]; then
       source ~/.psyrendust-update
 
-      if [[ -z "$LAST_PSYRENDUST_REMOTE_SHA" ]]; then
-        upgrade_psyrendust && return 0;
+      if [[ -z "$CURRENT_PSYRENDUST_LOCAL_SHA" ]]; then
+        update_psyrendust_local_sha && return 0;
       fi
 
-      if [[ $LAST_PSYRENDUST_REMOTE_SHA != $(current_psyrendust_local_sha) ]]; then
-        printf '\033[0;35m%s \033[0;31m%s\033[0m\n' "[Oh My Zsh Psyrendust]" "Updates found"
+      if [[ $CURRENT_PSYRENDUST_LOCAL_SHA != $(get_current_psyrendust_remote_sha) ]]; then
+        printf '\033[0;35m%s \033[0;31m%s\033[0m\n' "[Oh My Zsh Psyrendust]" "Updates found..."
         upgrade_psyrendust
+      else
+        update_psyrendust_local_sha
       fi
     else
       # create the psyrendust file
-      update_psyrendust_remote_sha
+      update_psyrendust_local_sha
     fi
   fi
 }
