d22c54ef1f5ec2ebd79689d1ed27451b5b7701a6
Larry Gordon <larry.gordon@xero.com>
2014-02-07 00:02:06 -0800

    Consolidated logic for updating repp's. Now checks the SHA's of each repo to see if an update is available.

--- plugins/psyrendust-auto-update/upgrade.zsh
+++ plugins/psyrendust-auto-update/upgrade.zsh
@@ -1,5 +1,22 @@
 #!/usr/bin/env zsh
+
+function _has-internet() {
+  wget -q --tries=10 --timeout=5 http://www.google.com -O /tmp/index.google &> /dev/null && echo 1
+}
+
+function _get-current-repo-remote-sha() {
+  cd "${1}"
+  echo $(git ls-remote $(git config remote.origin.url) HEAD | awk '{print $1}')
+}
+
+function _set-last-repo-update() {
+  cd "${1}"
+  echo "current_local_sha=$(git rev-parse HEAD)" > "$HOME/.psyrendust/last-repo-update-${2}"
+  printf '\033[0;35m%s \033[0;33m%s\033[0m\n' "[$HOME/.psyrendust/last-repo-update-${2}] " "Updated"
+}
+
 function _is-git-repo() {
+  cd "${1}"
   # check if we're in a git repo
   if [[ -d "./.git" ]]; then
     if [[ $(git rev-parse --is-inside-work-tree) == "true" ]]; then
@@ -19,128 +36,75 @@ function _update-repo() {
   # Check if the folder exists
   if [[ -d $1 ]]; then
     cd $1
-    # Check if we're in a git repo
-    if [[ -n $(_is-git-repo) ]]; then
-      _cleanup
-      if git pull --rebase origin master; then
-        # Run any repo specific updates
-        callback_function="${2:-}"
-        [[ $callback_function == '' ]] || $($2)
-        unset callback_function
-        echo 1
-      fi
-    else
-      echo 2
-    fi
-  else
-    echo 2
-  fi
-}
-
-function _update-psyrendust() {
-  # Run custom updates
-  if [[ -d $ZSH_CUSTOM ]]; then
-    cp "$ZSH_CUSTOM/templates/.zshrc" "$HOME/.zshrc"
-
-    if [[ -n $SYSTEM_IS_MAC ]]; then
-      # Replace .gitconfig
-      [[ -s "$ZSH_CUSTOM/templates/.gitconfig_mac" ]] && cp "$ZSH_CUSTOM/templates/.gitconfig_mac" "$HOME/.gitconfig"
-      # Check to see if .gitconfig-includes has been created
-      [[ -d "$HOME/.gitconfig-includes" ]] || mkdir "$HOME/.gitconfig-includes"
-    fi
-
-    if [[ -n $SYSTEM_IS_CYGWIN ]]; then
-      # Replace .gitconfig
-      [[ -s "$ZSH_CUSTOM/templates/.gitconfig_win" ]] && cp "$ZSH_CUSTOM/templates/.gitconfig_win" "$HOME/.gitconfig"
-      # Check to see if .gitconfig-includes has been symlinked in cygwin
-      [[ -d "/cygdrive/z/.gitconfig-includes" ]] && ln -sf "/cygdrive/z/.gitconfig-includes" "$HOME/.gitconfig-includes"
-    fi
-
-    # Copy over gitconfig templates
-    [[ -s "$ZSH_CUSTOM/templates/core.gitconfig" ]] && cp "$ZSH_CUSTOM/templates/core.gitconfig" "$HOME/.gitconfig-includes/core.gitconfig"
-    [[ -s "$ZSH_CUSTOM/templates/diff.gitconfig" ]] && cp "$ZSH_CUSTOM/templates/diff.gitconfig" "$HOME/.gitconfig-includes/diff.gitconfig"
-
-    # Check to see if user.gitconfig has been created
-    if [[ ! -s "$HOME/.gitconfig-includes/user.gitconfig" ]]; then
-      [[ -s "$ZSH_CUSTOM/templates/user.gitconfig" ]] && cp "$ZSH_CUSTOM/templates/user.gitconfig" "$HOME/.gitconfig-includes/user.gitconfig"
-    fi
-
-    # Check to see if a Git global user.name has been set
-    if [[ $(git config user.name) == "" ]]; then
-      printf '\033[0;31m%s\033[0m\n' "One time setup to configure your Git user.name"
-      printf '\033[0;35m%s\033[0;31m%s\033[0m\n' "Please enter your first and last name " "[First Last]: "
-      read GIT_USER_NAME_FIRST GIT_USER_NAME_LAST
-      echo "  name = ${GIT_USER_NAME_FIRST} ${GIT_USER_NAME_LAST}" >> "$HOME/.gitconfig-includes/user.gitconfig"
-      printf '\033[0;35m%s\033[0;36m%s\033[0m\n' "Git config user.name saved to: " "$HOME/.gitconfig-includes/user.gitconfig"
-    fi
-
-    # Check to see if a Git global user.email has been set
-    if [[ $(git config user.email) == "" ]]; then
-      printf '\033[0;31m%s\033[0m\n' "One time setup to configure your Git user.email"
-      printf '\033[0;35m%s\033[0;31m%s\033[0m\n' "Please enter your work email address " "[first.last@domain.com]: "
-      read GIT_USER_EMAIL
-      echo "  email = ${GIT_USER_EMAIL}" >> "$HOME/.gitconfig-includes/user.gitconfig"
-      printf '\033[0;35m%s\033[0;36m%s\033[0m\n' "Git config user.email saved to: " "$HOME/.gitconfig-includes/user.gitconfig"
+    _cleanup
+    if git pull --rebase origin master; then
+      echo 1
     fi
   fi
 }
 
-function _update-pure-theme() {
-  # Run custom updates
+
+# Post process update scripts
+# ----------------------------------------------------------
+function _oh-my-zsh-psyrendust-post-update() {
+  ppemphasis '       __                            __                                    __         __  '
+  ppemphasis ' ___  / /     __ _  __ __   ___ ___ / /     ___  ___ __ _________ ___  ___/ /_ _____ / /_ '
+  ppemphasis '/ _ \/ _ \   /  ` \/ // /  /_ /(_-</ _ \   / _ \(_-</ // / __/ -_) _ \/ _  / // (_-</ __/ '
+  ppemphasis '\___/_//_/  /_/_/_/\_, /   /__/___/_//_/  / .__/___/\_, /_/  \__/_//_/\_,_/\_,_/___/\__/  '
+  ppemphasis '                  /___/                  /_/       /___/                                  '
+  ppsuccess  'Hooray!'
+  _set-last-repo-update $@
+}
+
+function _oh-my-zsh-post-update() {
+  ppsuccess "Oh My Zsh has been updated!"
+  _set-last-repo-update $@
+}
+
+function _pure-theme-post-update() {
   if [[ -d $PURE_THEME ]]; then
     ln -sf "$HOME/.pure-theme/pure.zsh" "$ZSH_CUSTOM/themes/pure.zsh-theme"
   fi
+  ppsuccess "Pure Theme has been updated!"
+  _set-last-repo-update $@
 }
 
-function _update-zshrc-personal() {
-  # Run custom updates
-  [[ -d $ZSHRC_PERSONAL ]]
+function _zshrc-personal-post-update() {
+  ppsuccess "zshrc personal has been updated!"
+  _set-last-repo-update $@
 }
 
-function _update-zshrc-work() {
-  # Run custom updates
-  [[ -d $ZSHRC_WORK ]]
+function _zshrc-work-post-update() {
+  ppsuccess "zshrc work has been updated!"
+  _set-last-repo-update $@
 }
 
-function _process-update() {
-  printf '\033[0;35m%s\033[0m\n' "Processing updates..."
 
-  [[ -d "$ZSH_CUSTOM" ]] && printf '\033[0;35m%s\033[0m\n' "Oh My Zsh Psyrendust"
-  success_update_psyrendust=$(_update-repo "$ZSH_CUSTOM" _update-psyrendust)
-
-  [[ -d "$PURE_THEME" ]] && printf '\033[0;35m%s\033[0m\n' "Pure Theme"
-  success_update_pure_theme=$(_update-repo "$PURE_THEME" _update-pure-theme)
-
-  [[ -d "$ZSHRC_PERSONAL" ]] && printf '\033[0;35m%s\033[0m\n' "Personal zshrc"
-  success_update_zshrc_personal=$(_update-repo "$ZSHRC_PERSONAL" _update-zshrc-personal)
-
-  [[ -d "$ZSHRC_WORK" ]] && printf '\033[0;35m%s\033[0m\n' "Work zshrc"
-  success_update_zshrc_work=$(_update-repo "$ZSHRC_WORK" _update-zshrc-work)
-
-  [[ -n $success_update_psyrendust ]]    || printf '\033[0;31m%s\033[0m\n' ' - There was an error updating Oh My Zsh Psyrendust.'
-  [[ -n $success_update_pure_theme ]]     || printf '\033[0;31m%s\033[0m\n' ' - There was an error updating Pure Theme.'
-  [[ -n $success_update_zshrc_personal ]] || printf '\033[0;31m%s\033[0m\n' ' - There was an error updating zshrc-personal.'
-  [[ -n $success_update_zshrc_work ]]     || printf '\033[0;31m%s\033[0m\n' ' - There was an error updating zshrc-work.'
-
-
-  if [[ -n $success_update_psyrendust && -n $success_update_pure_theme && -n $success_update_zshrc_personal && -n $success_update_zshrc_work ]]; then
-    printf '\033[0;35m%s\033[0m\n' '       __                            __                                    __         __  '
-    printf '\033[0;35m%s\033[0m\n' ' ___  / /     __ _  __ __   ___ ___ / /     ___  ___ __ _________ ___  ___/ /_ _____ / /_ '
-    printf '\033[0;35m%s\033[0m\n' '/ _ \/ _ \   /  ` \/ // /  /_ /(_-</ _ \   / _ \(_-</ // / __/ -_) _ \/ _  / // (_-</ __/ '
-    printf '\033[0;35m%s\033[0m\n' '\___/_//_/  /_/_/_/\_, /   /__/___/_//_/  / .__/___/\_, /_/  \__/_//_/\_,_/\_,_/___/\__/  '
-    printf '\033[0;35m%s\033[0m\n' '                  /___/                  /_/       /___/                                  '
-    printf '\033[0;35m%s\033[0m\n' 'Hooray!'
-  else
-    printf '\033[0;31m%s\033[0m\n' 'Please try again later.'
-  fi
+# List repos we want to check and update
+# ----------------------------------------------------------
+while getopts ":a" opt; do
+  [[ $opt == "a" ]] && has_option=1
+done
+if [[ -n $has_option ]]; then
+  repos=(
+    "Oh My Zsh Psyrendust"
+    "Oh My Zsh"
+    "Pure Theme"
+    "zshrc Personal"
+    "zshrc Work"
+  )
+else
+  repos=(
+    "Oh My Zsh Psyrendust"
+    "zshrc Personal"
+    "zshrc Work"
+  )
+fi
 
 
-  unset success_update_psyrendust;
-  unset success_update_pure_theme;
-  unset success_update_zshrc_personal;
-  unset success_update_zshrc_work;
-}
 
+# Get the show going!
+# ----------------------------------------------------------
 if [[ -n $SYSTEM_IS_CYGWIN ]] && [[ -d "/cygdrive/z/.oh-my-zsh-psyrendust" ]]; then
   # Don't process updates for CYGWIN because we are in
   # Parallels and symlinking those folders to the this
@@ -150,5 +114,63 @@ if [[ -n $SYSTEM_IS_CYGWIN ]] && [[ -d "/cygdrive/z/.oh-my-zsh-psyrendust" ]]; t
   _update-zshrc-personal
   _update-zshrc-work
 else
-  _process-update
+  # Check and see if we have internet first before continuing on
+  if [[ -n $(_has-internet) ]]; then
+
+    # Source Pretty Print
+    [[ -f $ZSH_CUSTOM/plugins/pretty-print/pretty-print.plugin.zsh ]] && source $ZSH_CUSTOM/plugins/pretty-print/pretty-print.plugin.zsh
+
+    pppurple -i "Checking for updates"
+    for repo in $repos; do
+      # Create local variables to hold the namespace and the repo's root
+      name_space="$(echo $repo | tr '[:upper:]' '[:lower:]' | tr ' ' '-')"
+      repo_root="$HOME/.${name_space}"
+
+      pplightpurple -i "."
+
+      # Check if the repo folder exists
+      if [[ -d $repo_root ]]; then
+
+        # Check if we are in a repo
+        if [[ -n $(_is-git-repo "$repo_root") ]]; then
+
+          # Check if the last-repo-update file exists
+          if [[ -f "$HOME/.psyrendust/last-repo-update-${name_space}" ]]; then
+            source "$HOME/.psyrendust/last-repo-update-${name_space}"
+
+            # Set the current_local_sha if it doesn't exist
+            if [[ -z "$current_local_sha" ]]; then
+              _set-last-repo-update "$repo_root" "$name_space" && return 0;
+            fi
+
+            # Compare the local sha against the remote
+            if [[ $current_local_sha != $(_get-current-repo-remote-sha $repo_root) ]]; then
+              ppemphasis -i "[$repo] "
+              ppyellow "Updates found..."
+              update_successful=$(_update-repo "$repo_root")
+
+              if [[ -n $update_successful ]]; then
+                # Run any repo specific updates
+                _${name_space}-post-update "$repo_root" "$name_space"
+              else
+                pperror " - There was an error updating $repo."
+              fi
+
+              unset update_successful
+            fi
+
+          else
+            # Set last repo update
+            ppdanger "last-repo-update file does not exists"
+            _set-last-repo-update "$repo_root" "$name_space"
+          fi
+
+        fi
+
+      fi
+      unset name_space
+      unset repo_root
+    done
+
+  fi
 fi
