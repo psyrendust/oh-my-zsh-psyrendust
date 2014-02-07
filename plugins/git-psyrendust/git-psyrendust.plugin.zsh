96c84b0a3af2d0ccd62ff6ceda3a73fda1b02062
Larry Gordon <larry.gordon@xero.com>
2014-02-06 16:33:13 -0800

    Fixing coding convention and adds root as a remote to a few functions

--- plugins/git-psyrendust/git-psyrendust.plugin.zsh
+++ plugins/git-psyrendust/git-psyrendust.plugin.zsh
@@ -21,28 +21,31 @@ compdef _git gtaga=git-tag
 alias gbdel='git branch -D'
 compdef _git gbdel=git-branch-D
 
-alias gbfromhere='git_branch_from_here'
+alias gbfromhere='_git-branch-from-here'
 compdef _git gbromhere=git-checkout-b
 
-alias gcob='git_checkout_branch'
+alias gcob='_git-checkout-branch'
 compdef _git gcob=git-checkout-b
 
-alias gcobu='git_checkout_branch upstream'
+alias gcobu='_git-checkout-branch upstream'
 compdef _git gcob=git-checkout-b
 
-alias gmfrom='git_merge_from'
+alias gmfrom='_git-merge-from'
 compdef _git gmfrom=git-merge
 
-alias gmfromorigin='git_merge_from_origin'
+alias gmfromorigin='_git-merge-from-origin'
 compdef _git gmfromorigin=git-merge-origin
 
-alias gmfromupstream='git_merge_from_upstream'
+alias gmfromupstream='_git-merge-from-upstream'
 compdef _git gmfromupstream=git-merge-upstream
 
-alias gmclean='git_merge_clean'
+alias gmfromroot='_git-merge-from-root'
+compdef _git gmfromroot=git-merge-root
+
+alias gmclean='_git-merge-clean'
 compdef _git gmclean=git-rm
 
-alias gsdel='git_stash_delete'
+alias gsdel='_git-stash-delete'
 compdef _git gsdel=git-stash
 
 alias gfo='git fetch origin'
@@ -51,11 +54,20 @@ compdef _git gfo=git-fetch-origin
 alias gfu='git fetch upstream'
 compdef _git gfu=git-fetch-upstream
 
+alias gfr='git fetch root'
+compdef _git gfr=git-fetch-root
+
+alias ggpullr='git pull root $(git_branch_name)'
+compdef _git ggpullr=git-pull-root
+
 alias ggpullu='git pull upstream $(git_branch_name)'
 compdef _git ggpullu=git-pull-upstream
 
 alias ggpullo='git pull origin $(git_branch_name)'
 compdef _git ggpullo=git-pull-origin
+
+alias ggpushr='git push root $(git_branch_name)'
+compdef _git ggpushr=git-push-root
 
 alias ggpushu='git push upstream $(git_branch_name)'
 compdef _git ggpushu=git-push-upstream
@@ -63,45 +75,32 @@ compdef _git ggpushu=git-push-upstream
 alias ggpusho='git push origin $(git_branch_name)'
 compdef _git ggpusho=git-push-origin
 
-alias gindex='git_index'
-compdef _git gindex=git-rm-cached-r
+alias gcleanindex='_git-clean-index'
+compdef _git gcleanindex=git-rm-cached-r
 
-alias gcindex='git_clean_index'
-compdef _git gcindex=git-rm-cached-r
-
-alias gl='git_log_pretty_grep'
+alias gl='_git-log-pretty-grep'
 compdef _git glg=git-log-pretty
 
-alias glb='git_log_pretty_grep_begin'
+alias glb='_git-log-pretty-grep-begin'
 compdef _git glg=git-log-pretty
 
-alias gls='git_log_pretty_grep_begin_sublime'
+alias gls='_git-log-pretty-grep-begin-sublime'
 compdef _git glg=git-log-pretty
 
-# alias gfupdate='git_flow_update'
+# alias gfupdate='git-flow-update'
 
-function git_index() {
-  echo "Working, please be patient..."
-  local currentlocation=$PWD
-  cd $(git rev-parse --show-toplevel)
-  git rm --cached -r .
-  git reset HEAD --hard
-  git add .
-  cd "$currentlocation"
-}
-
-function git_branch_name() {
+function _git_branch_name() {
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
   echo "${ref#refs/heads/}"
 }
 
-function git_branch_from_here() {
+function _git-branch-from-here() {
   git checkout -b $1 $(git_branch_name)
 }
 
 # git checkout remote branch and track it
-function git_checkout_branch() {
+function _git-checkout-branch() {
   if [[ "$#" -ne 2 ]]; then
     git checkout -b $1 origin/$1
   else
@@ -110,46 +109,55 @@ function git_checkout_branch() {
 }
 
 # update target branch and merge it into current branch
-function git_merge_from() {
-  currentbranch=$(git_branch_name)
-  targetbranch=$1
-  echo "switching to branch $targetbranch"
-  gco -f $targetbranch
-  echo "updating branch $targetbranch"
+function _git-merge-from() {
+  current_branch=$(git_branch_name)
+  target_branch=$1
+  echo "switching to branch $target_branch"
+  gco -f $target_branch
+  echo "updating branch $target_branch"
   ggpull
-  echo "switching to branch $currentbranch"
-  gco -f $currentbranch
+  echo "switching to branch $current_branch"
+  gco -f $current_branch
   echo "stashing any changes"
   gsdel
-  echo "merging $targetbranch into $currentbranch"
-  gm $targetbranch
+  echo "merging $target_branch into $current_branch"
+  gm $target_branch
 }
 
 # update branch from origin and merge it into current branch
-function git_merge_from_origin() {
-  currentbranch=$(git_branch_name)
+function _git-merge-from-origin() {
+  current_branch=$(git_branch_name)
   echo "fetching from origin $1"
   gfo $1:$1
-  echo "merging origin $1 into $currentbranch"
+  echo "merging origin $1 into $current_branch"
   gm $1
 }
 
 # update branch from upstream and merge it into current branch
-function git_merge_from_upstream() {
-  currentbranch=$(git_branch_name)
+function _git-merge-from-upstream() {
+  current_branch=$(git_branch_name)
   echo "fetching from upstream $1"
   gfu $1:$1
-  echo "merging upstream $1 into $currentbranch"
+  echo "merging upstream $1 into $current_branch"
+  gm $1
+}
+
+# update branch from root and merge it into current branch
+function _git-merge-from-root() {
+  current_branch=$(git_branch_name)
+  echo "fetching from upstream $1"
+  gfr $1:$1
+  echo "merging root $1 into $current_branch"
   gm $1
 }
 
 # clean up and remove any *.orig files created from a merge conflict
-function git_merge_clean() {
-  find ./ -type f -name \*.orig -exec rm -f {} \;
+function _git-merge-clean() {
+  find $(git rev-parse --show-toplevel) -type f -name \*.orig -exec rm -f {} \;
 }
 
 # create a stash and delete it
-function git_stash_delete() {
+function _git-stash-delete() {
   # Let's make sure there is something to stash
   command git diff --quiet --ignore-submodules HEAD &>/dev/null;
   (($? == 1)) && git stash save && git stash drop stash@{0};
@@ -157,24 +165,28 @@ function git_stash_delete() {
 
 # remove everything from the index and then write both the index and the
 # working directory from git's database.
-function git_clean_index() {
+function _git-clean-index() {
+  echo "Working, please be patient..."
+  local current_location=$PWD
   if [ $# -eq 0 ]; then
-    dir="."
+    cd $(git rev-parse --show-toplevel)
   else
-    dir=$1
+    cd $1
   fi
-  git rm --cached -r $dir
-  git reset --hard
+  git rm --cached -r .
+  git reset HEAD --hard
+  git add .
+  cd "$current_location"
 }
 
-function git_log_pretty_grep() {
+function _git-log-pretty-grep() {
   git log --pretty=format:'%s' -i --grep='$(1)'
 }
 
-function git_log_pretty_grep_begin() {
+function _git-log-pretty-grep-begin() {
   git log --pretty=format:'%s' -i --grep='^$1'
 }
 
-function git_log_pretty_grep_begin_sublime() {
+function _git-log-pretty-grep-begin-sublime() {
   git log --pretty=format:'%s' -i --grep='^$1'
 }
