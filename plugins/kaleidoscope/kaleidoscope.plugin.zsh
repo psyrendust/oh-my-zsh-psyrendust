4bd8f734db40e5d34113c1bf816c56989123cf19
Larry Gordon <larry.gordon@xero.com>
2014-02-26 08:53:47 -0800

    Adding in SYSTEM_IS_VM check

--- plugins/kaleidoscope/kaleidoscope.plugin.zsh
+++ plugins/kaleidoscope/kaleidoscope.plugin.zsh
@@ -1,7 +1,7 @@
 ## Kaleidoscope plugin
 # Creates a ksdiff tool that can launch Kaleidoscope.app
 # from the Cygwin terminal in Parallels.
-if [[ -n $SYSTEM_IS_CYGWIN ]]; then
+if [[ -n $SYSTEM_IS_CYGWIN ]] && [[ -n $SYSTEM_IS_VM ]]; then
   local me=$(whoami)
   local _kaleidoscope_path="/cygdrive/c/Users/$me/AppData/Roaming/Parallels/Shared Applications/Kaleidoscope (Mac).exe"
   if [[ -f $_kaleidoscope_path ]]; then
