7a57ff9c1e935879055c33e8ef291777f992ee35
Larry Gordon <larry.gordon@xero.com>
2014-03-03 16:09:41 -0800

    Adjusting zsh-syntax-highlighting settings

--- tools/init-post-settings.zsh
+++ tools/init-post-settings.zsh
@@ -18,3 +18,40 @@ if [[ -n $SYSTEM_IS_CYGWIN ]]; then
     PROMPT=$(echo $PROMPT | tr "❯" "›")
   fi
 fi
+
+
+
+# Settings for zsh-syntax-highlighting plugin
+# ----------------------------------------------------------------------------------------------------------------------
+# ASSIGNMENTS                                                                 # DEFAULT VALUE         DESCRIPTION
+# ----------------------------------------------------------------------------------------------------------------------
+ZSH_HIGHLIGHT_STYLES[default]=none                                            # none                  : parts of the buffer that do not match anything
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red,bold                               # fg=red,bold           : unknown tokens / errors
+ZSH_HIGHLIGHT_STYLES[reserved-word]=yellow,bold                                    # fg=yellow             : shell reserved words
+
+ZSH_HIGHLIGHT_STYLES[alias]=fg=green                                          # fg=green              : aliases
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=green                                        # fg=green              : shell builtin commands
+ZSH_HIGHLIGHT_STYLES[command]=fg=green                                        # fg=green              : commands
+ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=green                               # none                  : command separation tokens
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=green                                     # fg=green,underline    : precommands (i.e. exec, builtin, ...)
+ZSH_HIGHLIGHT_STYLES[function]=fg=green                                       # fg=green              : functions
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=green                                 # fg=green              : hashed commands
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=green,bold                      # none                  : single hyphen options
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=green,bold                      # none                  : double hyphen options
+
+
+ZSH_HIGHLIGHT_STYLES[assign]=fg=magenta,bold                                  # none                  : variable assignments
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=magenta,bold                                # fg=blue               : globbing expressions
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=magenta                # fg=cyan               : dollar double quoted arguments
+
+
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=blue,bold                     # fg=yellow             : single quoted arguments
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=blue,bold                     # fg=yellow             : double quoted arguments
+
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=cyan,bold                       # none                  : backquoted expressions
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=cyan,bold                # fg=cyan               : back double quoted arguments
+ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=cyan,bold                          # fg=blue               : history expansion expressions
+
+ZSH_HIGHLIGHT_STYLES[path]=fg=blue,bold                                       # underline             : paths
+ZSH_HIGHLIGHT_STYLES[path_approx]=fg=blue,bold                                # fg=yellow,underline   : approximated paths
+ZSH_HIGHLIGHT_STYLES[path_prefix]=fg=blue,bold                                # underline             : path prefixes
