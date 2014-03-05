b69fe6f958c53449494c0124ded986363dbc96de
Larry Gordon <larry.gordon@xero.com>
2014-03-05 11:28:19 -0800

    psyrendust-auto-update plugin removed in favor of tools/auto-update.zsh

--- plugins/psyrendust-auto-update/upgrade.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-# Check for updates on initial load...
-if [[ -f $ZSH_CUSTOM/tools/auto-update.zsh ]]; then
-  (/usr/bin/env ZSH=$ZSH ZSH_CUSTOM=$ZSH_CUSTOM DISABLE_PSYRENDUST_AUTO_UPDATE=$DISABLE_PSYRENDUST_AUTO_UPDATE zsh $ZSH_CUSTOM/tools/auto-update.zsh &)
-fi
